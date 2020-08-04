348fe9e37720718b5210864b664d136392a0d194
mail21 <ismailidn1@gmail.com>
2020-08-04 16:05:41 +0700

    Updates

---
+++ build/precache-manifest.a4df8481ab62f7fab0d58828c07438ac.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8becf332c24436578d482f6f75ecaf9f",
+    "url": "/web-loker-app/index.html"
+  },
+  {
+    "revision": "c3ce86d63da581fdce67",
+    "url": "/web-loker-app/static/css/2.507660f8.chunk.css"
+  },
+  {
+    "revision": "d500383a365b74d3db86",
+    "url": "/web-loker-app/static/css/main.bc4f9f26.chunk.css"
+  },
+  {
+    "revision": "c3ce86d63da581fdce67",
+    "url": "/web-loker-app/static/js/2.e4ca7796.chunk.js"
+  },
+  {
+    "revision": "929b7d4d4832f8b3ea4c2e2434567c19",
+    "url": "/web-loker-app/static/js/2.e4ca7796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d500383a365b74d3db86",
+    "url": "/web-loker-app/static/js/main.e1958f96.chunk.js"
+  },
+  {
+    "revision": "5868276b5bc3c783d496",
+    "url": "/web-loker-app/static/js/runtime-main.e10f25b6.js"
+  },
+  {
+    "revision": "f2c4291a6dc14e02544e239ca7701c40",
+    "url": "/web-loker-app/static/media/Jumbotron.f2c4291a.png"
+  },
+  {
+    "revision": "57494e6bfd1d080da4d9dbd91d265e37",
+    "url": "/web-loker-app/static/media/ProfilePic.57494e6b.png"
+  },
+  {
+    "revision": "a73ec7484c8e30afdcd7ffa5c5b983e1",
+    "url": "/web-loker-app/static/media/blogPic.a73ec748.jpg"
+  },
+  {
+    "revision": "3d2eb39d393e0150bdb3f469ef3863ba",
+    "url": "/web-loker-app/static/media/blueArrow.3d2eb39d.png"
+  },
+  {
+    "revision": "31e2cd1c9688f7089d68f2f1680934b6",
+    "url": "/web-loker-app/static/media/html.31e2cd1c.png"
+  },
+  {
+    "revision": "a7908d6d5c05ca88260966b411c08052",
+    "url": "/web-loker-app/static/media/ig.a7908d6d.png"
+  },
+  {
+    "revision": "f17bac471577d40be182f960eee82364",
+    "url": "/web-loker-app/static/media/logo.f17bac47.png"
+  },
+  {
+    "revision": "90907cab919c02a956ebe166dc655a85",
+    "url": "/web-loker-app/static/media/twitter.90907cab.png"
+  }
+]);
